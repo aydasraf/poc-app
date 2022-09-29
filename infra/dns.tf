7a97334f42bfccd0266b2c92f2b602442ff89fd2
ayd.asraf <ayd.asraf@auto1.com>
2022-09-30 01:11:49 +0200

    Add All App

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "external" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "${local.service_name}.${terraform.workspace}"
+  name    = "${local.service_name}-${terraform.workspace}"
   type    = "A"
 
   alias {
